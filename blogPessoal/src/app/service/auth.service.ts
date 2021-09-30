d49c7733cb5e969485b737bb694fccbc3b78ead7
sabrinalvespaiva <sabrina.paiva14@hotmail.com>
2021-09-30 19:20:34 -0300

    componente inicio

--- blogPessoal/src/app/service/auth.service.ts
+++ blogPessoal/src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
 
@@ -14,11 +15,24 @@ export class AuthService {
   ) { }
 
   entrar(userLogin: UserLogin): Observable<UserLogin>{
-    return this.http.post<UserLogin>('https://backendblogpessoal.herokuapp.com/usuarios/logar', UserLogin)
+    return this.http.post<UserLogin>('https://backendblogpessoal.herokuapp.com/usuarios/logar', userLogin)
   }
 
   cadastrar(user: User): Observable<User>{
     return this.http.post<User>('https://backendblogpessoal.herokuapp.com/usuarios/cadastrar', user)
+  }
+
+  logado(){
+    //começo minha execução com ok falso
+    let ok: boolean = false
+
+    //se ok for diferente de vazio ele sera um true
+    if(environment.token != ''){
+      ok = true
+    }
+
+    //retorna o ok true o false, depende se ele vai entrar na condição
+    return ok
   }
 
 }
