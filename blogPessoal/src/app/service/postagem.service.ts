ea9bba459584f9be5535ef32af9ee2a6e69f1ecf
sabrinalvespaiva <sabrina.paiva14@hotmail.com>
2021-10-06 20:48:33 -0300

    delete postagem

--- blogPessoal/src/app/service/postagem.service.ts
+++ blogPessoal/src/app/service/postagem.service.ts
@@ -19,8 +19,20 @@ export class PostagemService {
     return this.http.get<Postagem[]>('https://blogpessoaldasasa.herokuapp.com/postagens', this.token)
   }
 
+  getByIdPostagem(id: number): Observable<Postagem>{
+    return this.http.get<Postagem>('https://blogpessoaldasasa.herokuapp.com/postagens', this.token)
+  }
+
   postPostagem(postagem: Postagem): Observable<Postagem>{
     return this.http.post<Postagem>('https://blogpessoaldasasa.herokuapp.com/postagens', postagem, this.token)
+  }
+
+  putPostagem(postagem: Postagem): Observable<Postagem>{
+    return this.http.put<Postagem>('https://blogpessoaldasasa.herokuapp.com/postagens', postagem, this.token)
+  }
+
+  deletePostagem(id: number){
+    return this.http.delete('https://blogpessoaldasasa.herokuapp.com/postagens/${id}', this.token)
   }
   
 }
